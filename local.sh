43f937bd722bb506055ee334c28329175fe7a1da
bradyawn <38081482+bradyawn@users.noreply.github.com>
2022-09-28 15:43:37 -0400

    Changes to local.sh for setup
    - Changed bang to run sh instead of bash (as done in class)
    - Commented out PYTHONPATH for initial setup, will comment in for later functionalities

--- local.sh
+++ local.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
 # run our server locally:
-PYTHONPATH=$(pwd):$PYTHONPATH
+# PYTHONPATH=$(pwd):$PYTHONPATH
 FLASK_APP=server.endpoints flask run --host=127.0.0.1 --port=8000
+
