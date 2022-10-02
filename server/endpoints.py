fe7b20bbc400661cc60c4ab224bd4383708cc5e7
agm316 <agm316@nyu.edu>
2022-10-02 00:21:48 -0700

    added new endpoint that scrapes a website

--- server/endpoints.py
+++ server/endpoints.py
@@ -3,12 +3,20 @@ This is the file containing all of the endpoints for our flask app.
 The endpoint called `endpoints` will return all available endpoints.
 """
 
+from http import HTTPStatus
+# import time
+import requests
 from flask import Flask
 from flask_restx import Resource, Api
+import werkzeug.exceptions as wz
+from bs4 import BeautifulSoup
+# import urllib3
 # import db.db as db
 
 app = Flask(__name__)
 api = Api(app)
+
+SCRAPE_WEBSITE = '/scrape'
 
 
 @api.route('/hello')
@@ -38,3 +46,25 @@ class Endpoints(Resource):
         endpoints = ''
         # sorted(rule.rule for rule in api.app.url_map.iter_rules())
         return {"Available endpoints": endpoints}
+
+
+@api.route(f'{SCRAPE_WEBSITE}/<path:website>')
+class ScrapeWebsite(Resource):
+    """
+    This class will scrape a given webpage with a recipe and returns the recipe
+    information in a list
+    """
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
+    def get(self, website):
+        """
+        The `get()` method get the html from the website and return
+        a dictionary with the recipe info
+        """
+        html_doc = requests.get(website).content
+        soup = BeautifulSoup(html_doc, 'html.parser')
+        recipe_name = soup.find(id="article-heading_1-0").get_text().strip()
+        if recipe_name == '':
+            raise wz.NotFound(f'{website} not found')
+        recipe_to_return = {"recipe_name": recipe_name}
+        return recipe_to_return
