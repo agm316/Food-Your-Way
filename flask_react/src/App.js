98c5ce0dea76023cc6babe6dc3fd0a6927bfa71b
bradyawn <38081482+bradyawn@users.noreply.github.com>
2023-02-02 15:30:53 -0500

    Started basic layout for main menu
    Search, add recipes, etc

--- flask_react/src/App.js
+++ flask_react/src/App.js
@@ -1,22 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import {
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+} from 'react-native';
+
+function goSearchRecipes()
+{
+}
+
+function goAddRecipes()
+{
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Food Your Way (Edit <code>src/App.js</code> and save to reload.) 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+	
+	<Button
+  	onPress={goSearchRecipes}
+  	title="Search Recipes"
+  	color="#841584"
+  	accessibilityLabel="Search for recipes"/>
+
+         <p>
+
+        </p>
+
+	<Button
+  	onPress={goAddRecipes}
+  	title="Add Recipes"
+  	color="#841584"
+  	accessibilityLabel="Add new recipes"/>
+
       </header>
     </div>
   );
