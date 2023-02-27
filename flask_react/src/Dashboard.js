c9cd4c7796fd7f7e962f02ee339b74b6fed4658b
bradyawn <38081482+bradyawn@users.noreply.github.com>
2023-02-27 15:11:05 -0500

    Update Dashboard.js

--- flask_react/src/Dashboard.js
+++ flask_react/src/Dashboard.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import {
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+} from 'react-native';
 
-export default function Dashboard() {
-  return(
-    <h2>Dashboard</h2>
-  );
-}
+const Dashboard = ({navigation, route}) => {
+  return <Text> Dashboard screen! {route.params.name}'s profile</Text>;
+};
+
+export default Dashboard;
