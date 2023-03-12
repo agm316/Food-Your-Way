c4ba09be78a554f33513e33504b40e32b1891d5d
agm316 <agm316@nyu.edu>
2023-03-12 16:24:39 -0700

    made SearchAllRec endpoint search for as many recipes as it can find on the allrecipes.com website given the search terms.

--- server/endpoints.py
+++ server/endpoints.py
@@ -82,6 +82,25 @@ recipe_suggestions = Namespace(RECIPE_SUGGESTIONS_NS, 'Recipe Suggestions')
 api.add_namespace(recipe_suggestions)
 
 
+def SearchLaterPages(website):
+    """
+    searches later pages for url's or recipes
+    and returns a list of them
+    """
+    len_url_start = len(ALL_REC_URL_START)
+    len_sub_url = len(URL_REC_SUB_TARGET)
+    html_doc = requests.get(website).content
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    link_lst = []
+    for link in soup.find_all('a', href=True):
+        link_i = link.get('href')
+        if (((link_i[0:len_url_start]) == ALL_REC_URL_START) and
+            ((link_i[len_url_start:(len_sub_url+len_url_start)]) ==
+             URL_REC_SUB_TARGET)):
+            link_lst.append(link.get('href'))
+    return link_lst
+
+
 @api.route('/hello')
 class HelloWorld(Resource):
     """
@@ -432,17 +451,37 @@ class SearchAllRec(Resource):
     This endpoint searches allrecipes.com for a given keyword and returns
     a list of url's to related pages
     so far this only returns one page
+    example
+    https://www.allrecipes.com/search?q=pesto+pizza
     second page example
     https://www.allrecipes.com/search?pizza=pizza&offset=24&q=pizza
+    https://www.allrecipes.com/search?pesto%20pizza=pesto%20pizza&offset=24&q=pesto%20pizza
+    third page example
+    https://www.allrecipes.com/search?pizza=pizza&offset=48&q=pizza
     """
     @api.response(HTTPStatus.OK, 'Success')
     @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
     def get(self, search_query):
         link_lst = []
+        query_str = ''
+        terms_url_ready = ""
         len_url_start = len(ALL_REC_URL_START)
         len_sub_url = len(URL_REC_SUB_TARGET)
         query_url = "https://www.allrecipes.com/search?q="
-        query_url = query_url + search_query.strip()
+        second_p_url = "https://www.allrecipes.com/search?"
+        subsequent_url_base = ''
+        term_lst = (search_query.strip()).split()
+        if (len(term_lst) == 0):
+            print("No Search Terms")
+            return []
+        else:
+            for x in range(len(term_lst)):
+                query_str = query_str + term_lst[x] + '+'
+                terms_url_ready = terms_url_ready + term_lst[x]
+                terms_url_ready = terms_url_ready + '%20'
+            query_str = query_str[:-1]
+            terms_url_ready = terms_url_ready[:-3]
+        query_url = query_url + query_str
         html_doc = requests.get(query_url).content
         soup = BeautifulSoup(html_doc, 'html.parser')
         for link in soup.find_all('a', href=True):
@@ -451,6 +490,23 @@ class SearchAllRec(Resource):
                ((link_i[len_url_start:(len_sub_url+len_url_start)]) ==
                URL_REC_SUB_TARGET)):
                 link_lst.append(link.get('href'))
+        second_p_url = second_p_url + terms_url_ready
+        second_p_url = second_p_url + '='
+        second_p_url = second_p_url + terms_url_ready
+        second_p_url = second_p_url + '&offset='
+        subsequent_url_base = second_p_url
+        y = 0
+        while (True):
+            offset = (y * 24) + 24
+            y = y + 1
+            temp_url = subsequent_url_base + str(offset)
+            temp_url = temp_url + '&q='
+            temp_url = temp_url + terms_url_ready
+            res_lst = SearchLaterPages(temp_url)
+            if (len(res_lst) == 0):
+                break
+            for x in range(len(res_lst)):
+                link_lst.append(res_lst[x])
         return link_lst
 
 
