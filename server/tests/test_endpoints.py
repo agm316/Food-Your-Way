6ee2b66a316937bddee8c2d480aaa69de8bbad2a
agm316 <113124960+agm316@users.noreply.github.com>
2023-10-20 12:31:47 -0700

    Update test_endpoints.py

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -99,8 +99,9 @@ def test_hellotest():
 
   """
   resp = TEST_CLIENT.post('/hellotest',
-                         data=json.dumps(TEST_DATA_1, indent=4, sort_keys=True, default=str))
-  input_data = json.dumps(TEST_DATA_1, indent=4, sort_keys=True, default=str)
+                         data=json.dumps(TEST_DATA_1, indent=4, sort_keys=False, default=str))
+  input_data = json.dumps(TEST_DATA_1, sort_keys=False, default=str)
+  # input_data = json.loads(TEST_DATA_1
   resp_json = resp.get_json()
   print(f'{input_data=}')
   print(f'{resp_json=}')
